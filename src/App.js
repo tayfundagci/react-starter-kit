3366d268f0d0ab915db9254db8450160fe30220e
tayfundagci <tayfundagci3517@gmail.com>
2022-07-03 12:13:20 +0300

    scss added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import { useEffect } from "react";
-import logo from "./logo.svg";
-import styles from "./App.module.css";
-import Test from "./Test";
-import { Title } from "./Components";
-import Bootstrap from "./Bootstrap";
-import Tailwind from "./Tailwind";
+import "./styles.scss";
 
 function App() {
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      // analytici bilgilendir
-    }
-  }, []);
-
   return (
-    <div className={styles.App}>
-      <Title>{process.env.NODE_ENV} git config user.email test</Title>
-      <Title theme="dark">{process.env.NODE_ENV} test</Title>
-      <p>{process.env.REACT_APP_API_URL}</p>
-      <Test />
-      {process.env.NODE_ENV === "production" && (
-        <>
-          <img src="logo192.png" alt="" />
-          <img src={logo} alt="" />
-        </>
-      )}
-      <Bootstrap />
-      <Tailwind />
-    </div>
+    <main>
+      <h1>tayfundagci.com</h1>
+    </main>
   );
 }
 
